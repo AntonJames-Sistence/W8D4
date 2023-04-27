a6dd5876440832ef513241e0855e96627b1b3458
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-04-27 14:14:28 -0400

    sum

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-"h"
+function sum() {
+    // const args = [...arguments];
+    let result = 0;
+    
+    // return console.log(arguments)
+    const mySlice = Array.prototype.slice.call(arguments);
+
+    mySlice.forEach((ele) => {result += ele});
+    console.log(result);
+};
+
+sum(2, 4, 5, 7);
+
+
+
